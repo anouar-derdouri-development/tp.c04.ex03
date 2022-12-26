58fee7a82a2e919275bb10e119784435c9ff153a
Anouar DERDOURI <118266778+anouar-derdouri-development@users.noreply.github.com>
2022-12-26 17:38:34 +0100

    Implement core functionality

--- app/src/main/java/com/example/tpc04ex03/GameActivity.java
+++ app/src/main/java/com/example/tpc04ex03/GameActivity.java
@@ -3,12 +3,51 @@ package com.example.tpc04ex03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+    int nombreAleatoire,
+            min = 10,
+            max = 30;
+    EditText etNombre;
+    TextView tvMessage;
+    Button btnOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        nombreAleatoire = new Random().nextInt((max - min) + 1) + min;
+
+        etNombre = findViewById(R.id.etNombre);
+        tvMessage = findViewById(R.id.tvMessage);
+        btnOK = findViewById(R.id.btnOK);
+
+        tvMessage.setText(String.format("Deviner un entier entre %d et %d", min, max));
+        Toast.makeText(this, "" + nombreAleatoire, Toast.LENGTH_SHORT).show();
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int nombreDonne = Integer.parseInt(etNombre.getText().toString());
+
+                    if (nombreDonne == nombreAleatoire) {
+                        Toast.makeText(GameActivity.this, "BRAVO", Toast.LENGTH_LONG).show();
+                        nombreAleatoire = new Random().nextInt((max - min) + 1) + min;
+                    }
+                    else
+                        Toast.makeText(GameActivity.this, "NON", Toast.LENGTH_LONG).show();
+                } catch (NumberFormatException e) {
+                    Toast.makeText(GameActivity.this, "Vous devez saisir un entier !!!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
