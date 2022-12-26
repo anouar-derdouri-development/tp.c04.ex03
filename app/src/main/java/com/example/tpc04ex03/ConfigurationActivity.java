02b17d69e3bca633dd2e2c4544b99be3043aa699
Raouane EDDOURRI <raouan.eddourri.development@gmail.com>
2022-12-26 09:37:46 -0800

    Implement configuration functionality

--- app/src/main/java/com/example/tpc04ex03/ConfigurationActivity.java
+++ app/src/main/java/com/example/tpc04ex03/ConfigurationActivity.java
@@ -3,12 +3,48 @@ package com.example.tpc04ex03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ConfigurationActivity extends AppCompatActivity {
+    EditText etMin, etMax;
+    Button btnConfOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        etMin = findViewById(R.id.etMin);
+        etMax = findViewById(R.id.etMax);
+        btnConfOK = findViewById(R.id.btnConfOK);
+
+        etMin.setText(String.valueOf(GameActivity.min));
+        etMax.setText(String.valueOf(GameActivity.max));
+
+        btnConfOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int minDonne = Integer.parseInt(etMin.getText().toString());
+                    int maxDonne = Integer.parseInt(etMax.getText().toString());
+
+                    if (minDonne == maxDonne)
+                        Toast.makeText(ConfigurationActivity.this, "min et max doivent être différente !!!", Toast.LENGTH_LONG).show();
+                    else if (minDonne > maxDonne)
+                        Toast.makeText(ConfigurationActivity.this, "min doit être inf. strictement à max !!!", Toast.LENGTH_LONG).show();
+                    else {
+                        GameActivity.min = minDonne;
+                        GameActivity.max = maxDonne;
+
+                        finish();
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(ConfigurationActivity.this, "Il faut saisir des entiers !!!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tpc04ex03/GameActivity.java
+++ app/src/main/java/com/example/tpc04ex03/GameActivity.java
@@ -2,6 +2,7 @@ package com.example.tpc04ex03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,12 +13,12 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
-    int nombreAleatoire,
-            min = 10,
+    int nombreAleatoire;
+    public static int min = 10,
             max = 30;
     EditText etNombre;
     TextView tvMessage;
-    Button btnOK;
+    Button btnOK, btnConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +30,10 @@ public class GameActivity extends AppCompatActivity {
         etNombre = findViewById(R.id.etNombre);
         tvMessage = findViewById(R.id.tvMessage);
         btnOK = findViewById(R.id.btnOK);
+        btnConfiguration = findViewById(R.id.btnConfiguration);
 
         tvMessage.setText(String.format("Deviner un entier entre %d et %d", min, max));
-        Toast.makeText(this, "" + nombreAleatoire, Toast.LENGTH_SHORT).show();
+
         btnOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,13 +43,30 @@ public class GameActivity extends AppCompatActivity {
                     if (nombreDonne == nombreAleatoire) {
                         Toast.makeText(GameActivity.this, "BRAVO", Toast.LENGTH_LONG).show();
                         nombreAleatoire = new Random().nextInt((max - min) + 1) + min;
-                    }
-                    else
+                    } else
                         Toast.makeText(GameActivity.this, "NON", Toast.LENGTH_LONG).show();
                 } catch (NumberFormatException e) {
                     Toast.makeText(GameActivity.this, "Vous devez saisir un entier !!!", Toast.LENGTH_LONG).show();
                 }
             }
         });
+
+        btnConfiguration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GameActivity.this, ConfigurationActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        tvMessage.setText(String.format("Deviner un entier entre %d et %d", min, max));
+
+        nombreAleatoire = new Random().nextInt((max - min) + 1) + min;
     }
 }
